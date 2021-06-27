3c99a5b4188b124f820e10da2fc81cf5f7f7db2f
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-27 16:55:54 +0530

    Added: EmployeeCheck

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,5 +3,21 @@ class EmployeeWage
 	public static void main(String[] args)
 	{
 	   System.out.println("Welcome to Employee Wage Computation");
+           Attendance employee = new Attendance();
+           employee.isPresent();
+
 	}
+}
+class  Attendance
+{
+   int IS_FULL_TIME = 1;
+   double empCheck = Math.floor(Math.random() * 10) % 2;
+   void isPresent()
+   {
+       if ( empCheck == IS_FULL_TIME )
+                    System.out.println("Employee is Present !");
+        else
+                    System.out.println("Employee is Absent !");
+   }
+
 }
